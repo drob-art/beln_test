14453281532ddd60b401519c809138b10280c49b
drob-art <fidj_24@mail.ru>
2017-07-19 15:43:43 +0300

    add redaktr

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -59,6 +59,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.test_workDataSet)).BeginInit();
@@ -171,6 +172,7 @@
             this.изменитьToolStripMenuItem.Name = "изменитьToolStripMenuItem";
             this.изменитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.изменитьToolStripMenuItem.Text = "Изменить";
+            this.изменитьToolStripMenuItem.Click += new System.EventHandler(this.изменитьToolStripMenuItem_Click);
             // 
             // удалитьToolStripMenuItem
             // 
@@ -304,11 +306,23 @@
             this.label6.Text = "Номер мобильный";
             this.label6.Visible = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(650, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "ОК";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(699, 331);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -371,6 +385,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -99,5 +99,58 @@ namespace WindowsFormsApplication1
             MessageBox.Show("Запись успешно удалена!", "Удаление", MessageBoxButtons.OK);
             this.telephone_directoryTableAdapter.Fill(this.test_workDataSet.telephone_directory);
         }
+
+        private void изменитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int cel_nom = dataGridView1.CurrentRow.Index;
+            textBox1.Visible = true; label1.Visible = true;
+            textBox2.Visible = true; label2.Visible = true;
+            textBox3.Visible = true; label3.Visible = true;
+            textBox4.Visible = true; label4.Visible = true;
+            textBox5.Visible = true; label5.Visible = true;
+            textBox6.Visible = true; label6.Visible = true;
+            button2.Visible = true;
+            textBox1.Text = (dataGridView1.Rows[cel_nom].Cells[0].Value).ToString();
+            textBox2.Text = (dataGridView1.Rows[cel_nom].Cells[1].Value).ToString();
+            textBox3.Text = (dataGridView1.Rows[cel_nom].Cells[2].Value).ToString();
+            textBox4.Text = (dataGridView1.Rows[cel_nom].Cells[3].Value).ToString();
+            textBox5.Text = (dataGridView1.Rows[cel_nom].Cells[4].Value).ToString();
+            textBox6.Text = (dataGridView1.Rows[cel_nom].Cells[5].Value).ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cel_nom = dataGridView1.CurrentRow.Index;
+            if (textBox1.Text == "" | textBox2.Text == "" | textBox3.Text == "" | textBox4.Text == "" | textBox5.Text == "" | textBox6.Text == "")
+            {
+                MessageBox.Show("Заполните все данные", "Ошибка", MessageBoxButtons.OK);
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\test\test_work.mdf;Integrated Security=True;Connect Timeout=30");
+                con.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter("UPDATE Telephone_directory SET FIO='" + textBox1.Text + "',Doljnost='" + textBox2.Text + "',Podrazdel='" + textBox3.Text + "',Number_work='" + textBox4.Text + "',Number_private='" + textBox5.Text + "',Number_mob='" + textBox6.Text + "' WHERE FIO = '" + dataGridView1.Rows[cel_nom].Cells[0].Value + "';", con);
+                SqlCommandBuilder cb = new SqlCommandBuilder(da);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "Telephone_directory");
+                this.telephone_directoryTableAdapter.Fill(this.test_workDataSet.telephone_directory);
+                MessageBox.Show("Запись успешно изменена", "Изменение", MessageBoxButtons.OK);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+                    textBox1.Visible = false; label1.Visible = false;
+                    textBox2.Visible = false; label2.Visible = false;
+                    textBox3.Visible = false; label3.Visible = false;
+                    textBox4.Visible = false; label4.Visible = false;
+                    textBox5.Visible = false; label5.Visible = false;
+                    textBox6.Visible = false; label6.Visible = false;
+                    button2.Visible = false;
+            }
+        }
     }
 }
