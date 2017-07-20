098ed8678c90c6179b3501bef547404633a57794
drob-art <fidj_24@mail.ru>
2017-07-20 09:38:48 +0300

    redakt diz add and viborka

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -50,7 +50,6 @@
             this.подразделенияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
@@ -68,6 +67,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.telephonedirectoryBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
@@ -234,14 +234,6 @@
             this.textBox2.TabIndex = 3;
             this.textBox2.Visible = false;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(224, 347);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.Visible = false;
-            // 
             // textBox4
             // 
             this.textBox4.Location = new System.Drawing.Point(330, 347);
@@ -390,6 +382,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Обслуживание",
@@ -411,11 +404,26 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Обслуживание",
+            "Управление",
+            "Производство"});
+            this.comboBox2.Location = new System.Drawing.Point(224, 346);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 22;
+            this.comboBox2.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(699, 380);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button4);
@@ -433,7 +441,6 @@
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
@@ -474,7 +481,6 @@
         private System.Windows.Forms.ToolStripMenuItem поискToolStripMenuItem;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
@@ -494,6 +500,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -27,7 +27,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "" | textBox2.Text == "" | textBox3.Text == "" | textBox4.Text == "" | textBox5.Text == "" | textBox6.Text == "")
+            if (textBox1.Text == "" | textBox2.Text == "" | comboBox2.Text == "" | textBox4.Text == "" | textBox5.Text == "" | textBox6.Text == "")
             {
                 MessageBox.Show("Заполните все данные", "Ошибка", MessageBoxButtons.OK);
             }
@@ -36,7 +36,7 @@ namespace WindowsFormsApplication1
                 SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\test\test_work.mdf;Integrated Security=True;Connect Timeout=30");
                 con.Open();
 
-                SqlDataAdapter da = new SqlDataAdapter("INSERT INTO Telephone_directory (FIO,Doljnost,Podrazdel,Number_work,Number_private,Number_mob) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "');", con);
+                SqlDataAdapter da = new SqlDataAdapter("INSERT INTO Telephone_directory (FIO,Doljnost,Podrazdel,Number_work,Number_private,Number_mob) VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + comboBox2.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "');", con);
                 SqlCommandBuilder cb = new SqlCommandBuilder(da);
                 DataSet ds = new DataSet();
                 da.Fill(ds, "Telephone_directory");
@@ -50,13 +50,13 @@ namespace WindowsFormsApplication1
                 {
                     textBox1.Text = "";
                     textBox2.Text = "";
-                    textBox3.Text = "";
+                    comboBox2.Text = "";
                     textBox4.Text = "";
                     textBox5.Text = "";
                     textBox6.Text = "";
                     textBox1.Visible = false; label1.Visible = false;
                     textBox2.Visible = false; label2.Visible = false;
-                    textBox3.Visible = false; label3.Visible = false;
+                    comboBox2.Visible = false; label3.Visible = false;
                     textBox4.Visible = false; label4.Visible = false;
                     textBox5.Visible = false; label5.Visible = false;
                     textBox6.Visible = false; label6.Visible = false;
@@ -67,7 +67,7 @@ namespace WindowsFormsApplication1
                 {
                     textBox1.Text = "";
                     textBox2.Text = "";
-                    textBox3.Text = "";
+                    comboBox2.Text = "";
                     textBox4.Text = "";
                     textBox5.Text = "";
                     textBox6.Text = "";
@@ -79,7 +79,7 @@ namespace WindowsFormsApplication1
         {
             textBox1.Visible = true; label1.Visible = true;
             textBox2.Visible = true; label2.Visible = true;
-            textBox3.Visible = true; label3.Visible = true;
+            comboBox2.Visible = true; label3.Visible = true;
             textBox4.Visible = true; label4.Visible = true;
             textBox5.Visible = true; label5.Visible = true;
             textBox6.Visible = true; label6.Visible = true;
@@ -105,14 +105,14 @@ namespace WindowsFormsApplication1
             int cel_nom = dataGridView1.CurrentRow.Index;
             textBox1.Visible = true; label1.Visible = true;
             textBox2.Visible = true; label2.Visible = true;
-            textBox3.Visible = true; label3.Visible = true;
+            comboBox2.Visible = true; label3.Visible = true;
             textBox4.Visible = true; label4.Visible = true;
             textBox5.Visible = true; label5.Visible = true;
             textBox6.Visible = true; label6.Visible = true;
             button2.Visible = true;
             textBox1.Text = (dataGridView1.Rows[cel_nom].Cells[0].Value).ToString();
             textBox2.Text = (dataGridView1.Rows[cel_nom].Cells[1].Value).ToString();
-            textBox3.Text = (dataGridView1.Rows[cel_nom].Cells[2].Value).ToString();
+            comboBox2.Text = (dataGridView1.Rows[cel_nom].Cells[2].Value).ToString();
             textBox4.Text = (dataGridView1.Rows[cel_nom].Cells[3].Value).ToString();
             textBox5.Text = (dataGridView1.Rows[cel_nom].Cells[4].Value).ToString();
             textBox6.Text = (dataGridView1.Rows[cel_nom].Cells[5].Value).ToString();
@@ -122,7 +122,7 @@ namespace WindowsFormsApplication1
         private void button2_Click(object sender, EventArgs e)
         {
             int cel_nom = dataGridView1.CurrentRow.Index;
-            if (textBox1.Text == "" | textBox2.Text == "" | textBox3.Text == "" | textBox4.Text == "" | textBox5.Text == "" | textBox6.Text == "")
+            if (textBox1.Text == "" | textBox2.Text == "" | comboBox2.Text == "" | textBox4.Text == "" | textBox5.Text == "" | textBox6.Text == "")
             {
                 MessageBox.Show("Заполните все данные", "Ошибка", MessageBoxButtons.OK);
             }
@@ -131,7 +131,7 @@ namespace WindowsFormsApplication1
                 SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\test\test_work.mdf;Integrated Security=True;Connect Timeout=30");
                 con.Open();
 
-                SqlDataAdapter da = new SqlDataAdapter("UPDATE Telephone_directory SET FIO='" + textBox1.Text + "',Doljnost='" + textBox2.Text + "',Podrazdel='" + textBox3.Text + "',Number_work='" + textBox4.Text + "',Number_private='" + textBox5.Text + "',Number_mob='" + textBox6.Text + "' WHERE FIO = '" + dataGridView1.Rows[cel_nom].Cells[0].Value + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter("UPDATE Telephone_directory SET FIO='" + textBox1.Text + "',Doljnost='" + textBox2.Text + "',Podrazdel='" + comboBox2.Text + "',Number_work='" + textBox4.Text + "',Number_private='" + textBox5.Text + "',Number_mob='" + textBox6.Text + "' WHERE FIO = '" + dataGridView1.Rows[cel_nom].Cells[0].Value + "';", con);
                 SqlCommandBuilder cb = new SqlCommandBuilder(da);
                 DataSet ds = new DataSet();
                 da.Fill(ds, "Telephone_directory");
@@ -139,13 +139,13 @@ namespace WindowsFormsApplication1
                 MessageBox.Show("Запись успешно изменена", "Изменение", MessageBoxButtons.OK);
                     textBox1.Text = "";
                     textBox2.Text = "";
-                    textBox3.Text = "";
+                    comboBox2.Text = "";
                     textBox4.Text = "";
                     textBox5.Text = "";
                     textBox6.Text = "";
                     textBox1.Visible = false; label1.Visible = false;
                     textBox2.Visible = false; label2.Visible = false;
-                    textBox3.Visible = false; label3.Visible = false;
+                    comboBox2.Visible = false; label3.Visible = false;
                     textBox4.Visible = false; label4.Visible = false;
                     textBox5.Visible = false; label5.Visible = false;
                     textBox6.Visible = false; label6.Visible = false;
